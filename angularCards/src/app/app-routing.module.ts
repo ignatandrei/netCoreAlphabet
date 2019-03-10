536c5561187dc0127a7226d055005d72f022e2c1
ignatandrei <ignatandrei@yahoo.com>
2019-03-10 19:36:44 +0200

    adding some visuals

--- angularCards/src/app/app-routing.module.ts
+++ angularCards/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NetCoreCardsComponent } from './net-core-cards/net-core-cards.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: '/cards', pathMatch: 'full'},
+{ path: 'cards',      component: NetCoreCardsComponent },
+{ path: 'about',      component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
