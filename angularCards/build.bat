ced4495c8f1dc8c34b5c60200bafaaf4698f4b59
ignatandrei <ignatandrei@yahoo.com>
2019-03-11 08:45:43 +0200

    artifact

--- angularCards/build.bat
+++ angularCards/build.bat
@@ -1,3 +1,6 @@
 ng build --prod --build-optimizer
 cd ..
+REM TODO: read https://angular.io/guide/deployment
+REM TODO: make a copy of docs/index.html and name it docs/404.html.
+REM TODO: https://github.com/angular-schule/angular-cli-ghpages
 node angularCards/node_modules/gh-pages/bin/gh-pages -d angularCards/dist/NetCoreAlphabet
