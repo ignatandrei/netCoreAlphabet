d556eae812ff5c977d49178d330cc2fb8edbf520
ignatandrei <ignatandrei@yahoo.com>
2019-03-11 22:17:46 +0200

    404

--- angularCards/build.bat
+++ angularCards/build.bat
@@ -1,6 +1,8 @@
-ng build --prod --build-optimizer
+call ng build --prod --build-optimizer
 cd ..
 REM TODO: read https://angular.io/guide/deployment
 REM TODO: make a copy of docs/index.html and name it docs/404.html.
 REM TODO: https://github.com/angular-schule/angular-cli-ghpages
+
+copy /Y angularCards\404GitHub.txt + angularCards\dist\NetCoreAlphabet\index.html angularCards\dist\NetCoreAlphabet\404.html
 node angularCards/node_modules/gh-pages/bin/gh-pages -d angularCards/dist/NetCoreAlphabet
