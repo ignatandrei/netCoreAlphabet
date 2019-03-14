15cb1eeb80b352746672e0a2719bf633f4233f05
ignatandrei <ignatandrei@yahoo.com>
2019-03-14 08:42:01 +0200

    no todos

--- angularCards/build.bat
+++ angularCards/build.bat
@@ -1,9 +1,6 @@
 call ng build --prod --build-optimizer
 cd ..
-REM TODO: read https://angular.io/guide/deployment
-REM TODO: make a copy of docs/index.html and name it docs/404.html.
-REM TODO: https://github.com/angular-schule/angular-cli-ghpages
-
+REM from read https://angular.io/guide/deployment
 copy /Y angularCards\404GitHub.txt + angularCards\dist\NetCoreAlphabet\index.html angularCards\dist\NetCoreAlphabet\404.html
 node angularCards/node_modules/gh-pages/bin/gh-pages -d angularCards/dist/NetCoreAlphabet
 rem robocopy angularCards/dist/NetCoreAlphabet cordovaCards/www/dist  /MIR /XD
