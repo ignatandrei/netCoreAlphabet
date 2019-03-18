d002ede40b9234b949720425927f08eecd60daf7
ignatandrei <ignatandrei@yahoo.com>
2019-03-18 21:34:59 +0200

    links and readme

--- README.md
+++ README.md
@@ -22,80 +22,85 @@ Feel free to make a PR request !
 
 
 
+
+
+
+
 ## A
-1. async / await
-2. Auto implemented properties
-3. Anonymous methods
-4. attributes
-5. Asp.Net
+1. async / await https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
+2. Auto implemented properties https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/auto-implemented-properties
+3. Anonymous methods https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/anonymous-methods
+4. attributes https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/attributes/
+5. ASP.Net https://docs.microsoft.com/en-us/aspnet/
 ## B
-1. Boxing
+1. Boxing https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/boxing-and-unboxing
 ## C
-1. CallerInfo attributes
-2. classes
+1. CallerInfo attributes https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/caller-information
 ## D
-1. Delegates
-2. Dynamic
+1. Delegates https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates/
+2. Dynamic https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/dynamic
 ## E
-1. Expando
-2. Expression bodied members
-3. Exception filters
-4. extension methods
-5. Entity Framework
-6. Events
+1. Expando https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa752492(v%3Dvs.85)
+2. Expression bodied members https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/expression-bodied-members
+3. Exception filters https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/when
+4. extension methods https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+5. Entity Framework https://docs.microsoft.com/en-us/ef/
+6. Events https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/events/
 ## G
-1. Generic Constraints
-2. Generics covariance / contravariance
-3. Generics
+1. Generic Constraints https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/constraints-on-type-parameters
+2. Generics covariance / contravariance https://docs.microsoft.com/en-us/dotnet/standard/generics/covariance-and-contravariance
+3. Generics https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/
 ## H
-1. health check
+1. health check https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-2.2
 ## I
-1. IDisosable
-2. Iterators
-3. interfaces
+1. IDisposable https://docs.microsoft.com/en-us/dotnet/api/system.idisposable
 ## J
-1.  jwt validation, authorization ,authentication 
+1. jwt validation, authorization ,authentication  https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/secure-net-microservices-web-applications/
 ## K
-1. kestrel
+1. kestrel https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-2.2
 ## L
-1. Local functions
-2. Linq
-3. Lambda
+1. Local functions https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/local-functions
+2. Linq https://docs.microsoft.com/en-us/dotnet/standard/using-linq
+3. Lambda https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/lambda-expressions
 ## M
-1. Memory&lt;T&gt;
+1. Memory&lt;T&gt; https://docs.microsoft.com/en-us/dotnet/api/system.memory-1.-ctor?view=netcore-2.2
 ## N
-1. Nameof
-2. Null propagator
-3. Nullable types
+1. Nameof https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/nameof
+2. Null conditional/propagator https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-conditional-operators
+3. Nullable types https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/nullable-types/
 ## O
-1. Out
+1. Out https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/out-parameter-modifier
 ## P
-1. Pattern matching
-2. Property initializers
-3. Partial types &#x2B; methods
+1. Pattern matching https://docs.microsoft.com/en-us/dotnet/csharp/pattern-matching
+2. Property initializers https://docs.microsoft.com/en-us/dotnet/csharp/properties
+3. Partial types https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/partial-type
+4. Partial methods https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/partial-method
 ## R
-1. Ref variables ( in function &#x2B; local )
-2. Razor
-3. Roslyn
+1. Ref variables in function  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref
+2. Ref variables in local https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref
+3. Razor https://docs.microsoft.com/en-us/aspnet/core/razor-pages/?view=aspnetcore-2.2&tabs=visual-studio
+4. Roslyn https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/
 ## S
-1. (.NET) Standard
-2. Span&lt;T&gt;
-3. string interpolation
-4. Static imports
-5. SignalR
-6. structs
-7. Static classes
+1. (.NET) Standard https://docs.microsoft.com/en-us/dotnet/standard/net-standard
+2. Span&lt;T&gt; https://docs.microsoft.com/en-us/dotnet/api/system.span-1?view=netcore-2.2
+3. string interpolation https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+4. Static imports https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-static
+5. SignalR https://docs.microsoft.com/en-us/aspnet/core/signalr/introduction?view=aspnetcore-2.2
+6. structs https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/struct
+7. Static classes https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/static-classes-and-static-class-members
 ## T
-1. tuple
-2. task
+1. tuple https://docs.microsoft.com/en-us/dotnet/csharp/tuples
+2. task https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task?view=netframework-4.7.2
 ## U
-1. Using for IDisposable
-2. unboxing
+1. Using for IDisposable https://docs.microsoft.com/en-us/dotnet/api/system.idisposable?view=netframework-4.7.2
+2. unboxing https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/boxing-and-unboxing
 ## V
-1. var
+1. var https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/var
 ## Y
-1. yield
+1. yield https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/yield
+## W
+1. when https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/when
 
 <small>
-    Autogenerated from <a href="https://azurestankins.azurewebsites.net/">Stankins</a> with the Razor .cshtml file SenderTrelloTo at 2019 Mar 17 17:24:04
+    Autogenerated from <a href=" https://azurestankins.azurewebsites.net/">Stankins</a> with the Razor .cshtml file SenderTrelloTo at 2019 Mar 18 19:15:09
 </small>
--- angularCards/src/app/data.ts
+++ angularCards/src/app/data.ts
@@ -4,13 +4,14 @@
 // in a CI scenario
 // See in  azure-pipelines.yml  - stankins.console execute
 
-
 export class Words {
     public constructor(init?: Partial<Words>) {
         if (init) { Object.assign(this, init); }
     }
     public name: string;
+    public officialLink: string;
     public cardsID: number;
+    public additionalLinks: string;
 }
 export class Cards {
     public constructor(init?: Partial<Cards>) {
@@ -27,215 +28,319 @@ export class Table1 {
     public name: string;
     public ID: number;
 }
-export { arrWords, arrCards, arrTable1, };
+export { arrWords, arrCards, arrTable1, }
 const arrWords = Array<Words>();
 arrWords.push({
     name: 'async / await',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/',
     cardsID: 1,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Auto implemented properties',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/auto-implemented-properties',
     cardsID: 1,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Anonymous methods',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/anonymous-methods',
     cardsID: 1,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'attributes',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/attributes/',
     cardsID: 1,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
-    name: 'Asp.Net',
+    name: 'ASP.Net',
+    officialLink: 'https://docs.microsoft.com/en-us/aspnet/',
     cardsID: 1,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Boxing',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/boxing-and-unboxing',
     cardsID: 2,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'CallerInfo attributes',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/caller-information',
     cardsID: 3,
-}); // end push toarrWords
-arrWords.push({
-    name: 'classes',
-    cardsID: 3,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Delegates',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/delegates/',
     cardsID: 4,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Dynamic',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/dynamic',
     cardsID: 4,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Expando',
+    officialLink: 'https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa752492(v%3Dvs.85)',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Expression bodied members',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/expression-bodied-members',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Exception filters',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/when',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'extension methods',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Entity Framework',
+    officialLink: 'https://docs.microsoft.com/en-us/ef/',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Events',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/events/',
     cardsID: 5,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Generic Constraints',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/constraints-on-type-parameters',
     cardsID: 6,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Generics covariance / contravariance',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/standard/generics/covariance-and-contravariance',
     cardsID: 6,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Generics',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/',
     cardsID: 6,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'health check',
+    officialLink: 'https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-2.2',
     cardsID: 7,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
-    name: 'IDisosable',
+    name: 'IDisposable',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/api/system.idisposable',
     cardsID: 8,
-}); // end push toarrWords
+    additionalLinks: '',
+    }); // end push toarrWords
 arrWords.push({
-    name: 'Iterators',
-    cardsID: 8,
-}); // end push toarrWords
-arrWords.push({
-    name: 'interfaces',
-    cardsID: 8,
-}); // end push toarrWords
-arrWords.push({
-    name: ' jwt validation, authorization ,authentication ',
+    name: 'jwt validation, authorization ,authentication ',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/secure-net-microservices-web-applications/',
     cardsID: 9,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'kestrel',
+    officialLink: 'https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-2.2',
     cardsID: 10,
-}); // end push toarrWords
+    additionalLinks: '',
+    }); // end push toarrWords
 arrWords.push({
     name: 'Local functions',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/local-functions',
     cardsID: 11,
-}); // end push toarrWords
+    additionalLinks: '',
+    }); // end push toarrWords
 arrWords.push({
     name: 'Linq',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/standard/using-linq',
     cardsID: 11,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Lambda',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/lambda-expressions',
     cardsID: 11,
-}); // end push toarrWords
+    additionalLinks: '',
+    }); // end push toarrWords
 arrWords.push({
     name: 'Memory<T>',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/api/system.memory-1.-ctor?view=netcore-2.2',
     cardsID: 12,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Nameof',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/nameof',
     cardsID: 13,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
-    name: 'Null propagator',
+    name: 'Null conditional/propagator',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-conditional-operators',
     cardsID: 13,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Nullable types',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/nullable-types/',
     cardsID: 13,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Out',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/out-parameter-modifier',
     cardsID: 14,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Pattern matching',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/pattern-matching',
     cardsID: 15,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Property initializers',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/properties',
     cardsID: 15,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
-    name: 'Partial types + methods',
+    name: 'Partial types',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/partial-type',
     cardsID: 15,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
-    name: 'Ref variables ( in function + local )',
+    name: 'Partial methods',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/partial-method',
+    cardsID: 15,
+    additionalLinks: '',
+}); // end push toarrWords
+arrWords.push({
+    name: 'Ref variables in function ',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref',
     cardsID: 16,
+    additionalLinks: '',
+}); // end push toarrWords
+arrWords.push({
+    name: 'Ref variables in local',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref',
+    cardsID: 16,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Razor',
+    officialLink: 'https://docs.microsoft.com/en-us/aspnet/core/razor-pages/?view=aspnetcore-2.2&tabs=visual-studio',
     cardsID: 16,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Roslyn',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/',
     cardsID: 16,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: '(.NET) Standard',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/standard/net-standard',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Span<T>',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/api/system.span-1?view=netcore-2.2',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'string interpolation',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated',
     cardsID: 17,
-}); // end push toarrWords
+    additionalLinks: '',
+    }); // end push toarrWords
 arrWords.push({
     name: 'Static imports',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-static',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'SignalR',
+    officialLink: 'https://docs.microsoft.com/en-us/aspnet/core/signalr/introduction?view=aspnetcore-2.2',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'structs',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/struct',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Static classes',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/static-classes-and-static-class-members',
     cardsID: 17,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'tuple',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/tuples',
     cardsID: 18,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'task',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task?view=netframework-4.7.2',
     cardsID: 18,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'Using for IDisposable',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/api/system.idisposable?view=netframework-4.7.2',
     cardsID: 19,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'unboxing',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/boxing-and-unboxing',
     cardsID: 19,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'var',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/var',
     cardsID: 20,
+    additionalLinks: '',
 }); // end push toarrWords
 arrWords.push({
     name: 'yield',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/yield',
     cardsID: 21,
+    additionalLinks: '',
+}); // end push toarrWords
+arrWords.push({
+    name: 'when',
+    officialLink: 'https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/when',
+    cardsID: 22,
+    additionalLinks: '',
 }); // end push toarrWords
 const arrCards = Array<Cards>();
 arrCards.push({
@@ -341,6 +446,11 @@ arrCards.push({
 arrCards.push({
     name: 'Y',
     ID: 21,
+    OwnerID: 1,
+}); // end push toarrCards
+arrCards.push({
+    name: 'W',
+    ID: 22,
     OwnerID: 1,
 }); // end push toarrCards
 const arrTable1 = Array<Table1>();
